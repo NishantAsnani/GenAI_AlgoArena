3839b0ee9951d8611f3e65dca24af4d0f2932f48
NishantAsnani <asnaninishant2@gmail.com>
2026-04-12 16:42:52 +0530

    Add problem controller and routes for managing problems

---
+++ be/routes/api/problem.routes.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const router=express.Router();
+const problemController=require('../../controllers/problem.controller');
+
+
+
+router.post('/add',problemController.addProblem);
+router.get('/',problemController.getAllProblems);
+router.get('/:id',problemController.getProblemById);
+router.put('/:id',problemController.updateProblem);
+router.delete('/:id',problemController.deleteProblem);
