5d87bff03273f120a69ed46543da3a637029df82
NishantAsnani <asnaninishant2@gmail.com>
2026-04-25 17:28:16 +0530

    feat: enhance auth middleware to handle invalid and expired tokens; update Redis connection logic for production

--- be/utils/queue.js
+++ be/utils/queue.js
@@ -1,11 +1,19 @@
 const { Queue } = require('bullmq');
 const IORedis = require('ioredis');
 
-const connection = new IORedis({
-  host: '127.0.0.1',
-  port: 6379,
-   maxRetriesPerRequest:null
-});
+const isProduction = process.env.NODE_ENV == 'development';
+
+const connection = isProduction
+  ? new IORedis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  maxRetriesPerRequest: null
+})
+  : new IORedis({
+      host: process.env.REDIS_HOST || '127.0.0.1',
+      port: process.env.REDIS_PORT || 6379,
+      maxRetriesPerRequest: null,
+    });
 
 const submissionQueue = new Queue('submission-queue', { connection });
 
