0f35d1dc685efd1bafbaf3609b404b66153ea1fd
NishantAsnani <asnaninishant2@gmail.com>
2026-04-25 19:19:48 +0530

    Minor queue fixes

--- be/utils/queue.js
+++ be/utils/queue.js
@@ -1,19 +1,19 @@
 const { Queue } = require('bullmq');
 const IORedis = require('ioredis');
 
-const isProduction = process.env.NODE_ENV == 'development';
+const isDevelopment = process.env.NODE_ENV == 'development';
 
-const connection = isProduction
-  ? new IORedis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-  maxRetriesPerRequest: null
-})
+const connection = isDevelopment
+  ? new IORedis(process.env.UPSTASH_REDIS_URL, {  
+      maxRetriesPerRequest: null,
+      tls: {}
+    })
   : new IORedis({
       host: process.env.REDIS_HOST || '127.0.0.1',
       port: process.env.REDIS_PORT || 6379,
       maxRetriesPerRequest: null,
     });
+
 
 const submissionQueue = new Queue('submission-queue', { connection });
 
