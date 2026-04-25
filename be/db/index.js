7bd241df9d5b9f5dd5dc5397b634a44e5515753b
NishantAsnani <asnaninishant2@gmail.com>
2026-04-25 17:02:30 +0530

    feat: implement admin seeder to create default administrator user

--- be/db/index.js
+++ be/db/index.js
@@ -5,7 +5,7 @@ const url = process.env.DB_URL || `mongodb://127.0.0.1:27017/${DB_NAME}`;
 const dbconnection =  async function () {
   try {
     await mongoose.connect(url);
-    console.log(`🔗 Connecting to MongoDB at ${url}...`);
+    console.log(`🔗 Connected to MongoDB`);
   } catch (err) {
     console.error("❌ Error connecting to MongoDB:", err);
     throw err; 
