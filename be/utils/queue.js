cf58ba74375225cb479c954f83d3618edeff3026
NishantAsnani <asnaninishant2@gmail.com>
2026-04-25 19:58:59 +0530

    minor fix for port

--- be/utils/queue.js
+++ be/utils/queue.js
@@ -6,7 +6,7 @@ const express=require('express');
 const app=express();
 
 app.get('/', (req, res) => res.send('Worker is running'));
-app.listen(process.env.PORT || 3001, () => console.log('Server alive'));
+app.listen(3001, () => console.log('Server alive'));
 
 
 const connection = isDevelopment
